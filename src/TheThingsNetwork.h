fb75edcbb25c51f6400573a174ba447f32ec39fc
nicolas <nicolas.dejean@epitech.eu>
2016-09-14 15:40:42 +0200

    defined debugSerialPrint/Ln for Serial and debugPrint/Ln for Stream to avoid warnings while compilating so in examples use debugSerialPrint/Ln and debugPrint/Ln in library

--- src/TheThingsNetwork.h
+++ src/TheThingsNetwork.h
@@ -23,6 +23,17 @@
 #define HEX_CHAR_TO_NIBBLE(c) ((c >= 'A') ? (c - 'A' + 0x0A) : (c - '0'))
 #define HEX_PAIR_TO_BYTE(h, l) ((HEX_CHAR_TO_NIBBLE(h) << 4) + HEX_CHAR_TO_NIBBLE(l))
 
+#ifdef DEBUG
+#define debugSerialPrintLn(...) { if (debugSerial) debugSerial.println(__VA_ARGS__); }
+#define debugSerialPrint(...) { if (debugSerial) debugSerial.print(__VA_ARGS__); }
+#define debugPrintLn(...) { if (debugStream) debugStream->println(__VA_ARGS__); }
+#define debugPrint(...) { if (debugStream) debugStream->print(__VA_ARGS__); }
+#else
+#define debugPrintLn(...)
+#define debugPrint(...)
+#endif
+
+
 class TheThingsNetwork
 {
   private:
