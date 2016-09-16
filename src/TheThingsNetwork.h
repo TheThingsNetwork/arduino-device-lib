9e039eb2ca41a497f37fe6b42eddcf0e6e9413e1
nicolas <nicolas.dejean@epitech.eu>
2016-09-16 16:40:29 +0200

    initialized debugStream to Serial by default, got ride of an argument in ttn.init()

--- src/TheThingsNetwork.h
+++ src/TheThingsNetwork.h
@@ -41,14 +41,14 @@ class TheThingsNetwork
   public:
     int downlinkPort;
     byte downlink[64];
-    void init(Stream& modemStream, Stream& debugStream);
+    void init(Stream& modemSerial);
     void reset(bool adr = true, int sf = DEFAULT_SF, int fsb = DEFAULT_FSB);
     bool personalize(const byte devAddr[4], const byte nwkSKey[16], const byte appSKey[16]);
     bool join(const byte appEui[8], const byte appKey[16]);
     int sendBytes(const byte* buffer, int length, int port = 1, bool confirm = false);
     int sendString(String message, int port = 1, bool confirm = false);
-    void setModemSerial(Stream& modemStream, Stream& replaceSerial);
-    void setDebugSerial(Stream& debugStream, Stream& replaceSerial);
+    void setModemSerial(Stream& replaceSerial);
+    void setDebugSerial(Stream& replaceSerial);
     void showStatus();
 };
 
