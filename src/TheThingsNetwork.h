a8e871088d77937069aa2c42edf4e52b448cffce
nicolas <nicolas.dejean@epitech.eu>
2016-09-29 16:51:42 +0200

    replaced tabs by spaces and removed commentary in the header

--- src/TheThingsNetwork.h
+++ src/TheThingsNetwork.h
@@ -12,11 +12,6 @@
 #define TTN_DEFAULT_FSB 2
 #define TTN_RETX 7
 
-// Set ADR off as it is currently not supported by The Things Network
-// The RN2xx3 module slows down to SF12 when no datarate commands are
-// sent by the network, so disabling ADR is a work-around to avoid
-// all the modules slowing down to SF12
-
 #define TTN_PWRIDX_868 1
 #define TTN_PWRIDX_915 5
 
