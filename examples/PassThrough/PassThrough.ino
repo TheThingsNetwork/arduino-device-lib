2c65615bec6840177a096f6ceaa2317b9a340e49
Fokke Zandbergen <mail@fokkezb.nl>
2016-10-10 15:41:56 +0200

    Update pass through to work with Node

--- examples/PassThrough/PassThrough.ino
+++ examples/PassThrough/PassThrough.ino
@@ -1,7 +1,7 @@
 /**
  * Use this sketch to talk directly to the LoRa module.
  * 
- * In Serial Monitor, select "Both NL & CR" and "9600 baud" in
+ * In Serial Monitor, select "Both NL & CR" and "115200 baud" in
  * the bottom right dropdowns and send command a command like:
  *
  * mac get deveui
@@ -12,8 +12,12 @@
 #define debugSerial Serial
 
 void setup() {
+  while(!debugSerial || !loraSerial);
+
+  debugSerial.begin(115200);
+  delay(1000);
+
   loraSerial.begin(57600);
-  debugSerial.begin(9600);
 }
 
 void loop() {
