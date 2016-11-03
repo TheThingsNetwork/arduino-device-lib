c28fa4bf6c5a7a706a80bd7cc7d349743d5faa9b
Fokke Zandbergen <mail@fokkezb.nl>
2016-11-03 10:15:27 +0100

    Add braces

--- examples/QuickStart/Encoder.js
+++ examples/QuickStart/Encoder.js
@@ -3,7 +3,9 @@ function Encoder(object, port) {
   // object to an array or buffer of bytes.
   var bytes = [];
     
-  if (port === 1) bytes[0] = object.led ? 1 : 0;
+  if (port === 1) {
+    bytes[0] = object.led ? 1 : 0;
+  }
     
   return bytes;
 }
