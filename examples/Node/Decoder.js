d5c94458c10d38626f8d97b0b191ee1fb73cd842
Fokke Zandbergen <mail@fokkezb.nl>
2016-12-23 14:40:26 +0100

    Add Node example for activation website, Align Node and Uno example for activation, turn on LED if all is OK

--- examples/Node/Decoder.js
+++ examples/Node/Decoder.js
@@ -5,6 +5,7 @@
  * 
  * {
  *   "event": "interval",
+ *   "device": "node",
  *   "battery": 4750,
  *   "light": 33,
  *   "temperature": 23.94
@@ -14,18 +15,24 @@
 function Decoder(bytes, port) {
   var decoded = {};
 
-  var ports = {
+  var events = {
     1: 'setup',
     2: 'interval',
     3: 'motion',
     4: 'button'
   };
 
-  decoded.event = ports[port];
+  if (port > 10) {
+    port = port - 10;
+    decoded.device = 'uno';
+  } else {
+    decoded.device = 'node';
+    decoded.battery = (bytes[0] << 8) + bytes[1];
+    decoded.light = (bytes[2] << 8) + bytes[3];
+    decoded.temperature = ((bytes[4] << 8) + bytes[5]) / 100;
+  }
 
-  decoded.battery = (bytes[0] << 8) + bytes[1];
-  decoded.light = (bytes[2] << 8) + bytes[3];
-  decoded.temperature = ((bytes[4] << 8) + bytes[5]) / 100;
+  decoded.event = events[port];
 
   return decoded;
 }
