012e6511281617deff3f88c08b76a78c3ca0034d
Peter Savnik <systems@savnik.dk>
2018-08-24 11:09:25 +0200

    Adding attempts to TheTHingsNetwork.h

--- src/TheThingsNetwork.h
+++ src/TheThingsNetwork.h
@@ -54,11 +54,11 @@ private:
   bool adr;
   char buffer[512];
   bool baudDetermined = false;
-  bool radioModuleInvalidState = false;  
+  bool radioModuleInvalidState = false;
   void (*messageCallback)(const uint8_t *payload, size_t size, port_t port);
 
   void clearReadBuffer();
-  size_t readLine(char *buffer, size_t size);
+  size_t readLine(char *buffer, size_t size, uint8_t attempts);
   size_t readResponse(uint8_t prefixTable, uint8_t indexTable, uint8_t index, char *buffer, size_t size);
   size_t readResponse(uint8_t table, uint8_t index, char *buffer, size_t size);
 
@@ -103,7 +103,7 @@ public:
   void wake();
   void saveState();
   void linkCheck(uint16_t seconds);
-  uint8_t getLinkCheckGateways();  
+  uint8_t getLinkCheckGateways();
   uint8_t getLinkCheckMargin();
 };
 
