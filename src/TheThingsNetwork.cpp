0fc3bc2ea128fe442871f8bc8ceac7606f225458
Peter Savnik <systems@savnik.dk>
2018-12-01 03:54:27 +0100

    Removing comment at readline function

--- src/TheThingsNetwork.cpp
+++ src/TheThingsNetwork.cpp
@@ -357,7 +357,7 @@ void TheThingsNetwork::clearReadBuffer()
   }
 }
 
-size_t TheThingsNetwork::readLine(char *buffer, size_t size, uint8_t attempts) // Default timeout value 10s and is set in class initiator
+size_t TheThingsNetwork::readLine(char *buffer, size_t size, uint8_t attempts)
 {
   size_t read = 0;
   while (!read && attempts--)
