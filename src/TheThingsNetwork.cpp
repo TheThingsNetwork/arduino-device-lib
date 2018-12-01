fe0c8ab94508f51ac00d8a388149ef39dc7d7a1f
Peter Savnik <systems@savnik.dk>
2018-12-01 04:12:09 +0100

    Replacing single qoute with double qoute to fix warning: character constant too long for its type

--- src/TheThingsNetwork.cpp
+++ src/TheThingsNetwork.cpp
@@ -367,7 +367,7 @@ size_t TheThingsNetwork::readLine(char *buffer, size_t size, uint8_t attempts)
   if (attempts<=0)
   { // If attempts is activated return 0 and set RN state marker
     this->needsHardReset = true; // Inform the application about the radio module is not responsive.
-    debugPrintLn('No response from RN module.');
+    debugPrintLn("No response from RN module.");
     
     return 0;
   }
